2cee81fa4843faaeb8ae73c0d3429ba857e96bcd
eckende <info@jandunker.de>
2020-06-07 17:18:49 +0200

    Create class Passenger and type enum

--- src/main/java/elsim/models/Passenger.java
+++ src/main/java/elsim/models/Passenger.java
@@ -1,5 +1,55 @@
 package main.java.elsim.models;
+import java.util.ArrayList;
 
-public class Passenger {
+/**
+ * Class for passengers that use elevators
+ * @see Load
+ * @see PassengerType
+ * @author jdunker
+ */
+public class Passenger extends Load {
+    public ArrayList<Item> items;
+    private PassengerType type;
+    private Floor floorStartingPoint;
+    private Floor floorDestination;
 
+    /**
+     * Manual constructor for Passenger Objects
+     * @param type Passenger type defined in enum
+     * @param mass Mass of the passenger in kg
+     * @param area Area needed for the passenger in m²
+     * @param floorStartingPoint Starting floor of passenger
+     * @param floorDestination Destination floor of passenger
+     */
+    public Passenger(PassengerType type, int mass, double area, Floor floorStartingPoint, Floor floorDestination){
+        this.type = type;
+        this.mass = mass;
+        this.area = area;
+        this.floorStartingPoint = floorStartingPoint;
+        this.floorDestination = floorDestination;
+    }
+
+    /**
+     * Get a passenger's list of items
+     * @return ArrayList of items
+     */
+    public ArrayList<Item> getItems(){
+        return this.items;
+    }
+
+    /**
+     * Get starting floor
+     * @return Floor object
+     */
+    public Floor getFloorStartingPoint() {
+        return floorStartingPoint;
+    }
+
+    /**
+     * Get destination floor
+     * @return Floor object
+     */
+    public Floor getFloorDestination() {
+        return floorDestination;
+    }
 }
---
+++ src/main/java/elsim/models/PassengerType.java
@@ -0,0 +1,14 @@
+package main.java.elsim.models;
+
+/**
+ * Enum that defines different classes of passengers.
+ *
+ * @see Passenger
+ * @author jdunker
+ */
+public enum PassengerType {
+    CHILD,
+    TEENAGER,
+    ADULT,
+    WHEELCHAIRUSER
+}
