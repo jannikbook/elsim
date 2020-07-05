917814a52d5c58ba42b091dc9dac144bfad2e5e9
Frank Wagner <frank.wagner98@arcor.de>
2020-07-05 11:41:02 +0200

    Added logger

---
+++ src/main/java/elsim/models/Logging.java
@@ -0,0 +1,9 @@
+package main.java.elsim.models;
+
+import java.util.logging.Logger;
+
+public class Logging {
+	
+	  public static final Logger log = Logger.getLogger( Logging.class.getName() );
+
+}
