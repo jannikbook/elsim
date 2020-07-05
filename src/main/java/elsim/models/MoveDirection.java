b55d81f3bc95e7b08b4df8b9042ebebe1e37f5b0
eckende <info@jandunker.de>
2020-07-05 20:55:13 +0200

    MoveDirection Javadoc

--- src/main/java/elsim/models/MoveDirection.java
+++ src/main/java/elsim/models/MoveDirection.java
@@ -1,5 +1,10 @@
 package main.java.elsim.models;
 
+/**
+ * MoveDirection is an enum for the current servicing direction of the elevator.
+ * {@Code Up} and {@Code Down} are used until there are no more floors to be serviced in the respective direction.
+ * When the elevator is awaiting new hall calls the state will be {@Code Hold}.
+ */
 public enum MoveDirection {
     Up,
     Down,
